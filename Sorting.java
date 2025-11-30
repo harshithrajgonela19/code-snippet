0b9079992561c366fbfcc7751f1740267e9ed603
harshithrajgonela19 <gharshithraj_cse2305e4@mgit.ac.in>
2025-11-30 19:05:37 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,1 +1,10 @@
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args){
+        int[] arr = {5, 2, 9, 1, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
 
